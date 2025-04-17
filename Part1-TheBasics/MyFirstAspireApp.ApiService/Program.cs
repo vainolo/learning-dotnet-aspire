f3a3dd4743b9fdef073ed6ca5531b7f320108512
Arieh Bibliowicz <arib@microsoft.com>
2025-04-17 20:43:04 +0300

    Application after weather logic change

--- Part1-TheBasics/MyFirstAspireApp.ApiService/Program.cs
+++ Part1-TheBasics/MyFirstAspireApp.ApiService/Program.cs
@@ -11,21 +11,29 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-var summaries = new[]
+string Summary(int forecast) => forecast switch
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    <= 0            => "Freezing",
+    > 0 and <= 10   => "Cool",
+    > 10 and <= 20  => "Mild",
+    > 20 and <= 30  => "Warm",
+    > 30            => "Scorching"
 };
+
+WeatherForecast GenerateForecast(int index)
+{
+    var temperature = Random.Shared.Next(-20, 55);
+    return new
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        temperature,
+        Summary(temperature)
+    );
+}
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    var forecast = Enumerable.Range(1, 5).Select(index => GenerateForecast(index)).ToArray();
     return forecast;
 });
 
